98f5d39f9046e5d1bf9853dbf06211df97b68618
maxboyko <maxboiko@divotek.com>
2025-07-29 20:29:34 +0300

    fix page layout

--- resources/views/filament/pages/settings.blade.php
+++ resources/views/filament/pages/settings.blade.php
@@ -1,11 +1,18 @@
 <x-filament::page>
-    <form wire:submit="save">
+    <form wire:submit="save" class="fi-sc-form">
         {{ $this->form }}
-
-        <div>
-            <x-filament::button type="submit">
-                Save
-            </x-filament::button>
+        <div class="fi-sc-component">
+            <div class="fi-sc-actions">
+                <div class="fi-ac fi-align-start">
+                    <x-filament::button type="submit">
+                        Save
+                    </x-filament::button>
+                    <button class="fi-btn fi-size-sm  fi-ac-btn-action" type="button" wire:loading.attr="disabled"
+                        x-on:click="document.referrer ? window.history.back() : Livewire.navigate('{{url()->previous()}}')">
+                        Cancel
+                    </button>
+                </div>
+            </div>
         </div>
     </form>
 </x-filament::page>

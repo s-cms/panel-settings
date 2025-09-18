bd4336312ae52af72473d374fe7b13f434cb15d4
maxboyko <maxboiko@divotek.com>
2025-09-18 12:06:23 +0300

    fix buttons translate and colors

--- resources/views/filament/pages/settings.blade.php
+++ resources/views/filament/pages/settings.blade.php
@@ -4,13 +4,13 @@
         <div class="fi-sc-component">
             <div class="fi-sc-actions">
                 <div class="fi-ac fi-align-start">
-                    <x-filament::button type="submit">
-                        Save
+                    <x-filament::button type="submit" size="sm">
+                        {{__('filament-panels::resources/pages/edit-record.form.actions.save.label')}}
                     </x-filament::button>
-                    <button class="fi-btn fi-size-sm  fi-ac-btn-action" type="button" wire:loading.attr="disabled"
+                    <x-filament::button type="button" wire:loading.attr="disabled" color="gray" size="sm"
                         x-on:click="document.referrer ? window.history.back() : Livewire.navigate('{{url()->previous()}}')">
-                        Cancel
-                    </button>
+                       {{ __('filament-panels::resources/pages/edit-record.form.actions.cancel.label')}}
+                    </x-filament::button>
                 </div>
             </div>
         </div>
